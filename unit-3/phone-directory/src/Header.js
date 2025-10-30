4b42906912c37ad168c6890366dc6be9a51bab3e
Sumit Kumar <mail.sumitkumar30@gmail.com>
2025-10-30 12:10:16 +0530

    Fix text alignment in Header component

--- unit-3/phone-directory/src/Header.js
+++ unit-3/phone-directory/src/Header.js
@@ -1,6 +1,6 @@
 const Header = function(props) {
     
-    const header = {alignItems:'center', backgroundColor: 'lightblue', padding: '10px'}
+    const header = {textAlign:'center', backgroundColor: 'lightblue', padding: '10px'}
     return (
     <div style={header}>
         <h1>{props.headerData}</h1>
